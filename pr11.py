87e7db24ead6c926330dbd8264e0942c5cac73dc
Lina <kokoshalina@gmail.com>
2024-05-16 22:15:29 +0300

    create feature layers for selections select by location (method - within a distance) select by attribute create layer with selected features get some atrribute data from objects in created layer add new field to created layer

--- pr11.py
+++ pr11.py
@@ -12,5 +12,10 @@ arcpy.CopyFeatures_management('facilities_lyr', out_path_copylayer)
 with arcpy.da.SearchCursor(out_path_copylayer, ["Shape", "ADDRESS", "NAME", "FACILITY"]) as cursor:
     for row in cursor:
         print('Geometry: {0}, Address: {1}, Name: {2}, Facility: {3}'.format(row[0], row[1], row[2], row[3]))
+#facilities_Distance_3000_lyr = r"E:\university\3 course\programming in gis\lections\pr6, lab 11\Programming_in_GIS_2024_L6_p11\Results\facilities_Distance_3000_lyr"
+arcpy.AddField_management(out_path_copylayer, 'C_NAME', 'DOUBLE')
+#arcpy.MakeFeatureLayer_management(out_path_copylayer, facilities_Distance_3000_lyr)
+#arcpy.management.AddJoin(facilities_Distance_3000_lyr, "NAME", 'facilities_lyr', "NAME")
+#arcpy.CalculateField_management(facilities_Distance_3000_lyr, "C_NAME", "!fc_facilities.FAC_ID!", "PYTHON_9.3")
 
 
