ffc6a32d6a0eb302bf38eaaf05473617a6c7d2b7
Lina <kokoshalina@gmail.com>
2024-05-16 22:09:10 +0300

    create feature layers for selections select by location (method - within a distance)

--- pr11.py
+++ pr11.py
@@ -5,4 +5,6 @@ fc_facilities = "facilities.shp"
 fc_zip = "zip.shp"
 arcpy.MakeFeatureLayer_management(fc_facilities, 'facilities_lyr')
 arcpy.MakeFeatureLayer_management(fc_zip, 'zip_lyr')
+arcpy.management.SelectLayerByLocation('facilities_lyr', 'WITHIN_A_DISTANCE', 'zip_lyr', '3000 Meters')
+
 
